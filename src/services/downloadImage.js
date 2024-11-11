43c6e81026fb0c5a9eed2a5a9535e6190e91e6ba
yusuf <yusuf210409@gmail.com>
2024-11-11 11:58:15 +0700

    fixing naming image issue

--- src/services/downloadImage.js
+++ src/services/downloadImage.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 async function download(data){
-  const response = await fetch(`https://storage.googleapis.com/testing-storage-aulia/test/${data.id}.${data.type.ext}`);
+  const response = await fetch(`https://storage.googleapis.com/testing-storage-aulia/test/${data.inferenceId}.${data.type.ext}`);
   const buffer = await response.buffer();
   return buffer;
 }
