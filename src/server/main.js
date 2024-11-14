c8f58b300fb66131a051fd629f1d61297521b9d7
yusuf <yusuf210409@gmail.com>
2024-11-14 16:07:49 +0700

    fixing bucket issue

--- src/server/main.js
+++ src/server/main.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const init = async () => {
   const server = Hapi.server({
     port:process.env.PORT || 8080,
-    host:'localhost',
+    // host:'localhost',
     routes: {
       cors:{
         origin:['*']
