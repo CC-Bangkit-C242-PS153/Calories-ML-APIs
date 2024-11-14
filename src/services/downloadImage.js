d80fc30e1dca29067a22bcc255c9d52014673022
yusuf <yusuf210409@gmail.com>
2024-11-14 15:42:42 +0700

    change bucket

--- src/services/downloadImage.js
+++ src/services/downloadImage.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 async function download(data){
-  const response = await fetch(`https://storage.cloud.google.com/fitcal/prediction/${data.inferenceId}.${data.type.ext}?authuser=4
+  const response = await fetch(`gs://fitcal/prediction/${data.inferenceId}.${data.type.ext}
 `);
   const buffer = await response.buffer();
   return buffer;
